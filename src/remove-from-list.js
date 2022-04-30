101e78656158b484d1da7e6ba8f13670d115cca6
Mazayw <mazayw@gmail.com>
2022-05-01 01:46:45 +0300

    feat: add remove-from-list solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let current = l;
+
+  while (current.next) {
+    if (current.next.value == k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  while (l && l.value == k) {
+    l = l.next;
+  }
+
+  return l;
+
 }
 
 module.exports = {
